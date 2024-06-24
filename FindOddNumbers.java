573ef145096dae0f6b4938808703333ada9be715
jeyasiva <37658389+jeyasiva@users.noreply.github.com>
2024-06-24 12:39:35 +0530

    Add files via upload

---
+++ FindOddNumbers.java
@@ -0,0 +1,18 @@
+package homework;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a = 10;
+		
+		for(int i =1; i<=a; i++) {
+			if(i%2==1) {
+				System.out.print(i+",");
+			}
+		}
+	}
+
+}
